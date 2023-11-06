df1204106276aa7b5980a60366932f4f0f6a45e8
personal <hasinmahmood01@gmail.com>
2023-11-06 13:18:22 +0000

    chore: store array in component state

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,11 +2,11 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 const IndexPage = () => {
-  const [battles, setBattles] = useState();
+  const [battles, setBattles] = useState([]);
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get("/api/update-db?playerTag=LGP89JU");
+        const response = await axios.get("/api/update-db?playerTag=CL9RULVRV");
         setBattles(response.data);
       } catch (error) {
         console.error("Failed to fetch battles", error);
