5f27963cd3cc8ab2ede7d933dd4af3e1e3b1d81e
personal <hasinmahmood01@gmail.com>
2023-11-06 01:26:43 +0000

    fix:Remove redundant condition for single API call

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,23 +4,17 @@ import axios from "axios";
 const IndexPage = () => {
   const [battles, setBattles] = useState();
   useEffect(() => {
-    let alreadyRan = false;
-    // Fetching data from the API endpoint
     const fetchData = async () => {
-      if (!alreadyRan) {
-        try {
-          const response = await axios.get("/api/update-db?playerTag=LGP89JU");
-          setBattles(response.data);
-        } catch (error) {
-          console.log(error);
-        }
+      try {
+        const response = await axios.get("/api/update-db?playerTag=LGP89JU");
+        setBattles(response.data);
+      } catch (error) {
+        console.error("Failed to fetch battles", error);
       }
     };
+
     fetchData();
-    return () => {
-      alreadyRan = true;
-    };
-  }, []); // The empty dependency array means this useEffect runs once when component mounts
+  }, []);
 
   return (
     <div>
