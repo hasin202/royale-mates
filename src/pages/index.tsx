d9b5b44ce7cec0bffe6c1d1545eb3c7045b96a87
personal <hasinmahmood01@gmail.com>
2023-11-06 13:24:07 +0000

    chore: make button to update db rows

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,22 +3,34 @@ import axios from "axios";
 
 const IndexPage = () => {
   const [battles, setBattles] = useState([]);
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get("/api/update-db?playerTag=CL9RULVRV");
-        setBattles(response.data);
-      } catch (error) {
-        console.error("Failed to fetch battles", error);
-      }
-    };
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     try {
+  //       const response = await axios.get("/api/update-db?playerTag=CL9RULVRV");
+  //       setBattles(response.data);
+  //     } catch (error) {
+  //       console.error("Failed to fetch battles", error);
+  //     }
+  //   };
 
-    fetchData();
-  }, []);
+  //   fetchData();
+  // }, []);
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get("/api/update-db?playerTag=CL9RULVRV");
+      setBattles(response.data);
+    } catch (error) {
+      console.error("Failed to fetch battles", error);
+    }
+  };
 
   return (
     <div>
       <h1>Index Page</h1>
+      <button onClick={fetchData} className="bg-white text-black">
+        UPDATE
+      </button>
       <p>{JSON.stringify(battles)}</p>
     </div>
   );
