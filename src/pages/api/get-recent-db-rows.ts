83e27be19e8a1db3cc99becac4742633bd4cf72e
personal <hasinmahmood01@gmail.com>
2023-11-07 14:36:38 +0000

    feat: return both recenet and all battles

--- src/pages/api/get-recent-db-rows.ts
+++ src/pages/api/get-recent-db-rows.ts
@@ -1,8 +1,10 @@
+import { handleError } from "@/lib/helpers/handle-error/handle-error";
 import prisma from "@/lib/prisma-client";
+import { DbRow } from "@/lib/types/types";
 
 export async function getRecentDbRows(
   playerTag: string | string[] | undefined
-) {
+): Promise<{ recentBattles: DbRow[]; allBattles: DbRow[] }> {
   try {
     console.log("Before querying the database"); // Log before the query
 
@@ -16,10 +18,10 @@ export async function getRecentDbRows(
       },
     });
 
-    if (battles.length === 0) return [];
-    return battles;
+    if (battles.length === 0) return { recentBattles: [], allBattles: [] };
+    const recentBattles = battles.slice(0, 31);
+    return { recentBattles: recentBattles, allBattles: battles };
   } catch (error) {
-    console.error("An error occurred:", error); // Log any errors that occur
-    throw { error: "An internal server error occurred" };
+    throw handleError(error, "something went wrong when getting database rows");
   }
 }
