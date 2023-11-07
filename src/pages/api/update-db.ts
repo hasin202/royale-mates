70bd4ff7c076e7a5bf2cba3de84e0673bf5330ad
personal <hasinmahmood01@gmail.com>
2023-11-07 14:38:12 +0000

    feat: add conditions to add recent battles

--- src/pages/api/update-db.ts
+++ src/pages/api/update-db.ts
@@ -2,29 +2,61 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import { fetchAndCleanBattles } from "./fetch-and-clean-battles";
 import { getRecentDbRows } from "./get-recent-db-rows";
 import { insertDbRows } from "./insert-db-rows";
+import { APIResponse, CustomError, UpdateDbResponse } from "@/lib/types/types";
+import { APIErrorHandler } from "@/lib/helpers/handle-error/handle-error";
 
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse
+  res: NextApiResponse<APIResponse<UpdateDbResponse>>
 ) {
   const playerTag = req.query?.playerTag;
 
   try {
-    console.log("enter update-db");
-    const friendlyBattles = await fetchAndCleanBattles(playerTag);
-    console.log("success hit royale endpoint");
+    let friendlyBattles = await fetchAndCleanBattles(playerTag);
+    const { recentBattles, allBattles } = await getRecentDbRows(playerTag);
 
-    const battles = await getRecentDbRows(playerTag);
-    console.log("success get db values");
-    if (battles.length === 0) {
-      console.log("battles length = 0");
+    // if theres no data in the db then add all friendly battles found
+    if (recentBattles.length === 0) {
       await insertDbRows(friendlyBattles);
-      return res
-        .status(200)
-        .json({ message: `inserted ${friendlyBattles.length} rows` });
+      return res.status(200).json({
+        body: {
+          success: true,
+          rowsAdded: `inserted ${friendlyBattles.length} rows`,
+        },
+      });
     }
-    return res.status(200).json({ in: "update-db", data: battles });
+
+    // store the most recent battle from the databases time stamp
+    const mostRecentBattleTime = recentBattles[0].battleTime;
+    // find the index for the corresponding battle withing friendly battles got from hitting the royale api
+    const indexOfBattleTimeInFriendlyBattles = friendlyBattles.findIndex(
+      (friendlyBattle) => friendlyBattle.battleTime === mostRecentBattleTime
+    );
+    // if the index is greater than 0 that means that there are new battles so edit friendly battles so that it only contains new battles
+    if (indexOfBattleTimeInFriendlyBattles > 0) {
+      friendlyBattles = friendlyBattles.slice(
+        0,
+        indexOfBattleTimeInFriendlyBattles
+      );
+      // add all new friendly battles found to the db
+      await insertDbRows(friendlyBattles);
+      return res.status(200).json({
+        body: {
+          success: true,
+          rowsAdded: `inserted ${friendlyBattles.length} rows`,
+        },
+      });
+    }
+
+    //if no new friendly battles are found then simply let the user know this and return all battles
+    return res.status(200).json({
+      body: {
+        success: false,
+        rowsAdded: "no rows added",
+        battles: allBattles,
+      },
+    });
   } catch (error) {
-    res.status(500).json({ message: "Oops an error occured" });
+    APIErrorHandler(res, error);
   }
 }
