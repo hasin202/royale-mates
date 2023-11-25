6492dec881e5f67572483e9648c6b017534905b7
hasin202 <88514256+hasin202@users.noreply.github.com>
2023-11-25 17:54:09 +0000

    feat: Update db table name and types (#42)
    * chore: rename table to UserBattles
    
    * chore: update table name
    
    * refactor:delete DbRow type, rather use prisma type

--- src/lib/types/battle-summaries-types.ts
+++ src/lib/types/battle-summaries-types.ts
@@ -1,11 +1,11 @@
-import { DbRow } from "./db-types";
-
+import { UsersBattles } from "@prisma/client";
 export type BattleSummary = {
   playerName: string;
+
   opponentName: string;
   playerWins: number;
   opponentWins: number;
-  battles: DbRow[];
+  battles: UsersBattles[];
 };
 
 export type BattleSummaries = BattleSummary[];
