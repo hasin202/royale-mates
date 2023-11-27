cda7b4154df7f91d0193f64844b0f7cf8fe119ee
personal <hasinmahmood01@gmail.com>
2023-11-27 20:12:15 +0000

    feat: add battle summaries to context

--- src/lib/types/battle-summaries-types.ts
+++ src/lib/types/battle-summaries-types.ts
@@ -1,11 +1,8 @@
 import { UsersBattles } from "@prisma/client";
 export type BattleSummary = {
   playerName: string;
-
   opponentName: string;
   playerWins: number;
   opponentWins: number;
   battles: UsersBattles[];
 };
-
-export type BattleSummaries = BattleSummary[];
