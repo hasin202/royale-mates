24219fafdf60e2da43392475a2a8c8589a5996e4
personal <hasinmahmood01@gmail.com>
2023-11-28 20:09:27 +0000

    feat: button to toggle visibility of battles

--- src/components/battle-summaries.tsx
+++ src/components/battle-summaries.tsx
@@ -1,35 +1,11 @@
 import { useGlobalState } from "@/lib/contexts/global-context";
 import IndividualBattles from "./individual-battles";
+import IndividiualSummaries from "./individual-summaries";
 
 const BattleSummaries = () => {
   const { battleSummaries } = useGlobalState();
-  const crownsRowStyle = "text-xl lg:text-3xl";
-  const playersNamesRowStyle = "text-2xl font-bold lg:text-4xl";
-
   return battleSummaries?.map((summary, i) => {
-    return (
-      <div
-        key={i}
-        className="px-8 py-2 flex flex-col items-center border rounded w-full"
-      >
-        <p className="font-bold text-xs">Overall Record</p>
-        <div className="grid grid-cols-[1fr_40px_1fr] gap-4 items-center w-full">
-          <div className="flex flex-col items-end justify-center">
-            <p className={crownsRowStyle}>{summary.playerWins}</p>
-            <p className={playersNamesRowStyle}>{summary.playerName}</p>
-          </div>
-          <div className="flex flex-col items-center justify-center">
-            <p className={crownsRowStyle}>-</p>
-            <p className={playersNamesRowStyle}>VS</p>
-          </div>
-          <div className="flex flex-col items-start justify-center">
-            <p className={crownsRowStyle}>{summary.opponentWins}</p>
-            <p className={playersNamesRowStyle}>{summary.opponentName}</p>
-          </div>
-        </div>
-        <IndividualBattles individualBattles={summary.battles} />
-      </div>
-    );
+    return <IndividiualSummaries key={i} summary={summary} />;
   });
 };
 
