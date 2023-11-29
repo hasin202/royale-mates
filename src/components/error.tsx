30b6cc08f96a6da17ba66a00a66482f125d8df32
personal <hasinmahmood01@gmail.com>
2023-11-29 09:54:32 +0000

    chore: add bottom margin to error popup

--- src/components/error.tsx
+++ src/components/error.tsx
@@ -3,7 +3,7 @@ import { useGlobalState } from "@/lib/contexts/global-context";
 const Error = () => {
   const { error } = useGlobalState();
   return (
-    <div className="p-4 bg-red-200 border rounded border-red-600 border-2 text-red-600">
+    <div className="p-4 bg-red-200 border rounded border-red-600 border-2 text-red-600 mb-4">
       <p className="text-xl font-bold">Oops</p>
       <p>{error?.message}</p>
       {error?.extraInfo && (
