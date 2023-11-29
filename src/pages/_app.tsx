0d2f85c65b8d81ac81f0acfc167f9e811eb2dbea
personal <hasinmahmood01@gmail.com>
2023-11-29 09:31:21 +0000

    feat: add vercel analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,13 @@
 import { GlobalStateProvider } from "@/lib/contexts/global-context";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <GlobalStateProvider>
       <Component {...pageProps} />
+      <Analytics />
     </GlobalStateProvider>
   );
 }
