63e9d4eb5a05f9902429f9b27b514e204ed33ae9
personal <hasinmahmood01@gmail.com>
2023-12-10 13:14:12 +0000

    fix: use cleaned tags in cron job

--- src/pages/api/cron.ts
+++ src/pages/api/cron.ts
@@ -1,17 +1,25 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import prisma from "@/lib/prisma-client";
 import apiLogic from "@/lib/helpers/update-db-api-logic/update-db-api-logic";
+import { cleanUserTag } from "@/lib/helpers/clean-user-tag/clean-user-tag";
 
 export default async function handler(
   request: NextApiRequest,
   response: NextApiResponse
 ) {
-  const uniquePlayerTags = await prisma.usersBattles.findMany({
-    select: {
-      playerTag: true,
-    },
-    distinct: ["playerTag"],
-  });
-  uniquePlayerTags.forEach(async (name) => await apiLogic(name.playerTag));
-  return response.status(200).send("completed cron");
+  try {
+    const uniquePlayerTags = await prisma.usersBattles.findMany({
+      select: {
+        playerTag: true,
+      },
+      distinct: ["playerTag"],
+    });
+    const cleanedPlayerTags = uniquePlayerTags.map((tag) =>
+      cleanUserTag(tag.playerTag)
+    );
+    cleanedPlayerTags.forEach(async (tag) => await apiLogic(tag));
+    return response.status(200).send("completed cron");
+  } catch (error) {
+    return response.status(400).send(error);
+  }
 }
